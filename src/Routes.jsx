759a1cd2c3796979543e00c3f33d68fe597e77bc
gangadharkr <gangadharkr27@gmail.com>
2024-04-02 17:55:49 +0000

    Code Synced by DhiWise

---
+++ src/Routes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+import Homepage from "pages/Homepage";
+
+const ProjectRoutes = () => {
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "*", element: <NotFound /> },
+    {
+      path: "homepage",
+      element: <Homepage />,
+    },
+  ]);
+
+  return element;
+};
+
+export default ProjectRoutes;
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const shapes = {
+  round: "rounded-[24px]",
+};
+const variants = {
+  fill: {
+    black_900: "bg-black-900 text-white-A700_87",
+  },
+};
+const sizes = {
+  xs: "h-[48px] px-6 text-base",
+};
+
+const Input = React.forwardRef(
+  (
+    {
+      className = "",
+      name = "",
+      placeholder = "",
+      type = "text",
+      children,
+      label = "",
+      prefix,
+      suffix,
+      onChange,
+      shape,
+      variant = "fill",
+      size = "xs",
+      color = "black_900",
+      ...restProps
+    },
+    ref,
+  ) => {
+    const handleChange = (e) => {
+      if (onChange) onChange(e?.target?.value);
+    };
+
+    return (
+      <>
+        <div
+          className={`${className} flex items-center justify-center gap-[35px] text-white-A700_87 tracking-[0.10px] text-base bg-black-900 rounded-[24px]  ${(shape && shapes[shape]) || ""} ${variants[variant]?.[color] || variants[variant] || ""} ${sizes[size] || ""}`}
+        >
+          {!!label && label}
+          {!!prefix && prefix}
+          <input ref={ref} type={type} name={name} onChange={handleChange} placeholder={placeholder} {...restProps} />
+          {!!suffix && suffix}
+        </div>
+      </>
+    );
+  },
+);
+
+Input.propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  label: PropTypes.string,
+  prefix: PropTypes.node,
+  suffix: PropTypes.node,
+  shape: PropTypes.oneOf(["round"]),
+  size: PropTypes.oneOf(["xs"]),
+  variant: PropTypes.oneOf(["fill"]),
+  color: PropTypes.oneOf(["black_900"]),
+};
+
+export { Input };
